1eac38f2eb336c9cc08d5785120a0bf10b7f62a2
Pedro Bittencourt <pedrohanza432@gmail.com>
2024-09-24 11:28:23 -0300

    Update no projeto de primeiro trabalho
    coloquei algumas informações!

--- projeto primeiro trabalho.c
+++ projeto primeiro trabalho.c
@@ -1,16 +1,16 @@
-
+//Trabalho realizado por Pedro Hanza Bittencourt
 
 
 
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
-#include <windows.h> //biblioteca que diz q esse codÌgo È pra windows
+#include <windows.h> //biblioteca que diz q esse cod√≠go √© pra windows
 
 main(void){
 		setlocale(LC_ALL,""); // pra suportar acentos
 	
-	// vari·veis
+	// vari√°veis
 	int i, qtd;
     float n1[100], n2[100], media[100];
     char sdt[100][100]; 
@@ -18,15 +18,15 @@ main(void){
     
 SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);// muda a cor do printf pra AZUL
 printf("---------------------------- \n Cadastro de estudantes \n ---------------------------- \n");
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // muda a cor do printf para a padr„o de novo
-	printf("Quantos estudantes ser„o cadastrados?");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // muda a cor do printf para a padr√£o de novo
+	printf("Quantos estudantes ser√£o cadastrados?");
 	scanf("%i", &qtd);
 	
 
 	for( i=0; i<qtd; i++){
 	
-	printf("Digite o nome do %d∫ estudante: \n", i + 1);// coloquei i+1 pois no FOR i=0
-	scanf("%s", sdt[i]);// usamos sdt[i] pra ele armazenar o primeiro espaÁo de memoria pra esse estudante
+	printf("Digite o nome do %d¬∫ estudante: \n", i + 1);// coloquei i+1 pois no FOR i=0
+	scanf("%s", sdt[i]);// usamos sdt[i] pra ele armazenar o primeiro espa√ßo de memoria pra esse estudante
 
 
 printf("Primeira nota do semestre: \n");
@@ -45,18 +45,18 @@ printf("Primeira nota do semestre: \n");
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
 		printf("------------------------------------------- \n"); // pra separar bonitinho os cadastros
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // alteramos a cor do cadastro 
-		printf("Nome do estudante: %s\n", sdt[i]); // aqui a gente imprime o espaÁo de memÛria que foi escrito pelo user l· em cima
+		printf("Nome do estudante: %s\n", sdt[i]); // aqui a gente imprime o espa√ßo de mem√≥ria que foi escrito pelo user l√° em cima
         printf("Primeira nota %.2f\n", n1[i]);
         printf("Segunda nota %.2f\n", n2[i]);
-        printf("MÈdia: %.2f\n", media[i]);
+        printf("M√©dia: %.2f\n", media[i]);
         if (media[i] >= 7.0){
         	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11); // trocar a cor pra verde!
 			printf("Aprovado! \n");
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);} // dps voltar pra padr„o do sistema, pra deixar sÛ o aprovado verde
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);} // dps voltar pra padr√£o do sistema, pra deixar s√≥ o aprovado verde
         	else {
         		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);// trocar a cor pra vermelho!
 				printf("Reprovado! \n");
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // dps voltar pra padr„o do sistema, pra deixar sÛ o reprovado vermelho
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15); // dps voltar pra padr√£o do sistema, pra deixar s√≥ o reprovado vermelho
 			}
 		}
 }
